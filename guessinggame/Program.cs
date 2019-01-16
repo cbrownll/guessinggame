02945bdbce053934d118bad42ab88a68f4e9cab5
cbrownll <cbrown7745@gmail.com>
2019-01-16 12:15:59 -0500

    3rd commit

--- guessinggame/Program.cs
+++ guessinggame/Program.cs
@@ -6,9 +6,24 @@ namespace guessinggame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Goodbye");
-        }
+            Console.WriteLine("Please guess a number between 1 and 10.");
+            string userGuess = Console.ReadLine();
+            Console.WriteLine("You guessed: " + userGuess);
 
+            // if they guess 7
+            if (userGuess.Equals("7"))
+            {
+                // then they win
+                Console.WriteLine("You win!");
+            } else
+            {
+                // otherwise they lose
+                Console.WriteLine("You Lost");
+            }
+
+            // then they win
+            // otherwise they lose
+
+        }
     }
 }
