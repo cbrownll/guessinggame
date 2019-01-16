b73d991230871d3ea41c317927844a773f1dec7e
cbrownll <cbrown7745@gmail.com>
2019-01-16 11:34:24 -0500

    translate

--- guessinggame/Program.cs
+++ guessinggame/Program.cs
@@ -7,6 +7,8 @@ namespace guessinggame
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Goodbye");
         }
+
     }
 }
